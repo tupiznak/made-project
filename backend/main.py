d8beac2876825e903bf661a4e60efe170a9affcf
tupiznak <akej-vonavi@mail.ru>
2022-09-23 20:13:26 +0300

    prepare db

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,55 @@
+import json
+
 from fastapi import FastAPI
+import os
+import datetime
+from mongoengine import *
+
+connect(name='test', host=os.environ.get('MONGODB_URI', None) or 'database')
+
+
+class BlogPost(Document):
+    title = StringField(required=True, max_length=200)
+    posted = DateTimeField(default=datetime.datetime.utcnow)
+    tags = ListField(StringField(max_length=50))
+    meta = {'allow_inheritance': True}
+
+
+class TextPost(BlogPost):
+    content = StringField(required=True)
+
+
+class LinkPost(BlogPost):
+    url = StringField(required=True)
+
 
 app = FastAPI()
 
+
 @app.get("/", tags=["Root"])
 async def read_root():
-  return { 
-    "message": "Welcome to my notes application, use the /docs route to proceed"
-   }
+    return {
+        "message": "Welcome to my notes application, use the /docs route to proceed"
+    }
+
+
+@app.get("/db/create", tags=["Root"])
+async def db_create():
+    post1 = TextPost(title='Using MongoEngine', content='See the tutorial')
+    post1.tags = ['mongodb', 'mongoengine']
+    post1.save()
+
+    return {
+        "message": "Welcome to my notes application, use the /docs route to proceed"
+    }
+
+
+@app.get("/db/read")
+async def db_read():
+    contents = []
+    for post in BlogPost.objects:
+        contents.append(post.content)
+
+    return {
+        "message": json.dumps(contents)
+    }
---
+++ build.sh
@@ -0,0 +1,4 @@
+cd backend
+docker build -t made22t4/backend:1.0 .
+cd ../frontend
+docker build -t made22t4/frontend:1.0 .
