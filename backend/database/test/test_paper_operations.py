24996b6b5a93d201fb87e569f4733ead954580c1
tupiznak <akej-vonavi@mail.ru>
2022-09-28 18:22:12 +0000

    made substr, filter, tests

--- backend/database/test/test_paper_operations.py
+++ backend/database/test/test_paper_operations.py
@@ -13,9 +13,9 @@ def db():
 @pytest.fixture
 def some_data(db):
     p1 = PaperOperations.create(Paper(_id='q', title='gtrgdtg', abstract='grtgrt'))
-    p2 = PaperOperations.create(Paper(_id='q2', title='gtrgdtg', abstract='grtgrt', year=2002))
-    p3 = PaperOperations.create(Paper(_id='q22', title='gtrgdtg', abstract='grtgrt'))
-    p4 = PaperOperations.create(Paper(_id='222', title='gg', abstract='grtgrt', year=2002))
+    p2 = PaperOperations.create(Paper(_id='q2', title='gtrgdtg', abstract='xa', year=2002))
+    p3 = PaperOperations.create(Paper(_id='q22', title='gtrgdtg', abstract='grtgrt kjfwe ewr'))
+    p4 = PaperOperations.create(Paper(_id='222', title='gg', abstract='wer', year=2002))
     return p1, p2, p3, p4
 
 
@@ -58,3 +58,9 @@ def test_filter(some_data):
     assert set(PaperOperations.filter(dict(year=2002))) == {some_data[1], some_data[3]}
     assert PaperOperations.filter(dict(year=2002, title='gg')) == [some_data[3]]
     assert PaperOperations.filter(dict(year=2002), exclude_paper=dict(title='gg')) == [some_data[1]]
+
+
+def test_sub_str_abstract(some_data):
+    assert set(PaperOperations.find_sub_string_in_abstract('we')) == {some_data[2], some_data[3]}
+    assert set(PaperOperations.find_sub_string_in_abstract('we', chunk_size=1)) == \
+           {some_data[2]} or {some_data[3]}
