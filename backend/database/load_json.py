35be81522fdc4bb25516ba80ae5047536d5b2570
tupiznak <akej-vonavi@mail.ru>
2022-09-28 04:29:32 +0000

    made json parser gen

--- backend/database/load_json.py
+++ backend/database/load_json.py
@@ -1,9 +1,11 @@
 import json
 import re
 import string
+import logging
+from pprint import pprint
 
 FILE_PATH = '../../../../mp_data/dblpv13.json'
-import logging
+FILE_LINES_COUNT = 409129302
 
 logging.basicConfig(level=logging.NOTSET)
 logger = logging.getLogger('json_parser')
@@ -21,7 +23,10 @@ def custom_replaces(line: str):
     return line
 
 
-def parse_json(file_path: str = FILE_PATH, previous_parsed_documents: int = 0):
+def parse_json(file_path: str = FILE_PATH,
+               previous_parsed_documents: int = 0,
+               log_period: int = 10000):
+    curr_lines_count = 1
     with open(file_path) as f:
         f.readline()  # first array open line "["
         documents_count = 0
@@ -30,6 +35,7 @@ def parse_json(file_path: str = FILE_PATH, previous_parsed_documents: int = 0):
             document_lines = ''
             while True:
                 line = f.readline()
+                curr_lines_count += 1
                 line = custom_replaces(line)
                 document_lines += line
                 if documents_count >= previous_parsed_documents - 1:
@@ -46,11 +52,19 @@ def parse_json(file_path: str = FILE_PATH, previous_parsed_documents: int = 0):
                 continue
             document_lines = re.sub(r'NumberInt\((.+)\)', r'(\1)', document_lines)
             logger.debug(document_lines.encode())
-            logger.warning(document_lines[1220:1240])
             logger.warning(documents_count)
-            logger.info(json.loads(document_lines))
+            json_doc = json.loads(document_lines)
+            logger.info(json_doc)
+
+            file_progress = curr_lines_count / FILE_LINES_COUNT
+            if documents_count % log_period == 0:
+                logger.error(f'file progress: {file_progress * 100:3.2f}%, '
+                             f'documents uploaded: {documents_count}')
+            yield json_doc
 
 
 if __name__ == '__main__':
-    logger.setLevel(level=logging.DEBUG)
-    parse_json()
+    logger.setLevel(level=logging.ERROR)
+    for doc_json in parse_json():
+        # pprint(doc_json)
+        pass
