26cdd9856dab7a39b2498de97e5bea6356c2b8e4
tupiznak <akej-vonavi@mail.ru>
2022-10-04 15:14:39 +0000

    upd front, fix fastapi

--- frontend/site/app.vue
+++ frontend/site/app.vue
@@ -2,23 +2,39 @@
 v-container
   v-row
     v-col
-      v-btn(@click="sendToDB") count
+      v-btn(@click="paperAmount") paper amount
+    v-col
+      v-btn(@click="authorAmount") author amount
+    v-col
+      v-btn(@click="venueAmount") venue amount
   v-row
     v-col
-      v-chip {{count}}
+      v-chip {{paperCount}}
+    v-col
+      v-chip {{authorCount}}
+    v-col
+      v-chip {{venueCount}}
 
 </template>
 
 <script setup>
 import { ref } from "vue";
-const count = ref("---")
+const paperCount = ref("---")
+const authorCount = ref("---")
+const venueCount = ref("---")
 const config = useRuntimeConfig()
 
-const sendToDB = async () => {
-    console.log(config.serverUrl)
-    await $fetch(`${config.serverUrl}/db/create`)
-    const data = await $fetch(`${config.serverUrl}/db/read`)
-    count.value = data.message
+const paperAmount = async () => {
+    const data = await $fetch(`${config.serverUrl}/database/paper/total_size`)
+    paperCount.value = data
+}
+const authorAmount = async () => {
+    const data = await $fetch(`${config.serverUrl}/database/author/total_size`)
+    authorCount.value = data
+}
+const venueAmount = async () => {
+    const data = await $fetch(`${config.serverUrl}/database/venue/total_size`)
+    venueCount.value = data
 }
 
 </script>
