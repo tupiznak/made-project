59bdaf79a28fa2727838e8bcbf97ee35c598df48
tupiznak <akej-vonavi@mail.ru>
2022-10-05 13:29:42 +0000

    add gauge totals metric

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,34 @@
 import mongoengine.errors
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from prometheus_client import Gauge
 from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 from app.routes.database import database_router
 from database.connection import connect
 
+from app.routes.database.paper import paper_operations
+from app.routes.database.author import author_operations
+from app.routes.database.venue import venue_operations
+
 _ = connect
+
+papers_count = Gauge('papers_count', 'Count of papers')
+author_count = Gauge('author_count', 'Count of author')
+venue_count = Gauge('venue_count', 'Count of venue')
+
+
+def metric_request(request: Request):
+    papers_count.set(paper_operations.total_size())
+    author_count.set(author_operations.total_size())
+    venue_count.set(venue_operations.total_size())
+    return handle_metrics(request)
+
 
 app = FastAPI()
 app.add_middleware(PrometheusMiddleware)
-app.add_route("/metrics", handle_metrics)
+app.add_route("/metrics", metric_request)
 
 
 @app.exception_handler(mongoengine.errors.NotUniqueError)
