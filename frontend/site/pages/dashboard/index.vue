adcca737cd2b02b2a810ecad38989be617502a9a
tupiznak <akej-vonavi@mail.ru>
2022-10-11 14:59:51 +0000

    add papers full abstract

---
+++ frontend/site/pages/dashboard/index.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+v-card
+  v-layout
+    v-app-bar
+      template(v-slot:prepend)
+        v-app-bar-nav-icon
+      v-app-bar-title Dashboard
+      v-text-field(v-model="search", label="Search Abstract", single-line)
+      v-btn(icon, @click="find")
+        v-icon mdi-magnify
+      v-btn(icon)
+        v-icon mdi-dots-vertical
+    v-main
+      v-container(fluid)
+        v-row(dense)
+          v-col(v-for="n in filteredPapers", :key="n", cols="12")
+            v-card.mx-auto(variant="outlined")
+              v-card-item
+                div
+                  v-container.pa-0(fluid)
+                    v-row(dense)
+                      v-col(cols=11)
+                        .text-h6.mb-1
+                          | {{ n.title }}
+                      v-col
+                        .text-overline.mb-1 {{ n.year }}
+                  .text-caption {{ n.abstract }}
+              v-card-actions
+                v-btn(variant="outlined")
+                  | details
+                v-spacer
+                v-btn(
+                  size="small",
+                  color="surface-variant",
+                  variant="text",
+                  icon="mdi-heart"
+                )
+</template>
+
+
+<script setup>
+import { ref } from "vue";
+const search = ref("");
+const config = useRuntimeConfig();
+const filteredPapers = ref([]);
+
+const paperAmount = async () => {
+  const data = await $fetch(`${config.serverUrl}/database/paper/total_size`);
+  paperCount.value = data;
+};
+
+const find = async () => {
+  const data = await $fetch(
+    `${config.serverUrl}/database/paper/abstract_substring?sub_string=${search.value}&chunk_size=10`,
+    { method: "POST" }
+  );
+  filteredPapers.value = data;
+  console.log(data);
+};
+// watch(search, async (search) => {
+//   console.log(search);
+//   const data = await $fetch(
+//     `${config.serverUrl}/database/paper/abstract_substring?sub_string=${search}&chunk_size=10`,
+//     { method: "POST" }
+//   );
+//   console.log(data);
+// });
+</script>
--- frontend/site/plugins/vuetify.js
+++ frontend/site/plugins/vuetify.js
@@ -1,3 +1,5 @@
+import "@mdi/font/css/materialdesignicons.min.css"
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -6,6 +8,9 @@ export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+      defaultSet: 'mdi'
+    }
   })
 
   nuxtApp.vueApp.use(vuetify)
