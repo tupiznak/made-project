c0c986b0d0c028005bac7ab7b321ed628dc15b2b
tupiznak <akej-vonavi@mail.ru>
2022-10-18 12:24:42 +0000

    fix list mongo

--- backend/database/db_objects/author.py
+++ backend/database/db_objects/author.py
@@ -8,6 +8,6 @@ class Author(Document):
     gid = StringField()
     oid = StringField()
     orgid = StringField()
-    papers = ListField(StringField)
+    papers = ListField(StringField())
 
     meta = {'strict': False, 'db_alias': 'citations'}
