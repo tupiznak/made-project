1a9250de42d92ec5be1b6165e17e4039a55129c7
tupiznak <akej-vonavi@mail.ru>
2022-12-10 18:03:25 +0000

    upd logger

--- backend/database/vectorisation_pipeline/set_author_embeddings.py
+++ backend/database/vectorisation_pipeline/set_author_embeddings.py
@@ -7,12 +7,16 @@ from pymongo import UpdateMany
 from pymongo.database import Database
 
 from database.connection import citations_db
-from enities.title_params import read_vectorizing_pipeline_params
+from database.split_train_test import DeltaTimeHandler, DeltaTimeFormatter
+
+# logging.basicConfig(level=logging.NOTSET)
+# database_changer_logger = logging.getLogger('database_changer')
+# database_changer_logger.setLevel(level=logging.DEBUG)
+OBJECTS_COUNT = 5354308
 
 logging.basicConfig(level=logging.NOTSET)
-database_changer_logger = logging.getLogger('database_changer')
-database_changer_logger.setLevel(level=logging.DEBUG)
-OBJECTS_COUNT = 5354308
+logger = logging.getLogger('database_changer')
+logger.setLevel(level=logging.DEBUG)
 
 
 def get_many_gen(database, collection: str, chunk_size: int):
@@ -28,26 +32,21 @@ def get_many_gen(database, collection: str, chunk_size: int):
 
 
 def writer(database: Database, doc_list: list[dict], model: fasttext.FastText):
-    # collection_test = COLLECTION_TEST
     author_col = database.get_collection('author')
 
-    # database.get_collection(collection_test).insert_many(doc_list)
     # print(doc_list)
     author_col.bulk_write([UpdateMany(
         filter={'_id': {'$in': el['authors']}},
         update={'$set': {
             f'vectorized_papers.{el["_id"]}': model.get_word_vector(el["title"]).tolist()}})
-        for el in doc_list if el.get('authors', None) is not None])
+        for el in doc_list if el.get('authors', None) is not None and el.get('title', None) is not None])
 
 
 @click.command("set_author_embedding")
 @click.argument("config_path")
 def set_author_embedding(config_path: str):
-    params = read_vectorizing_pipeline_params(
-        config_path).author_embeddings_params
-    batch_size = params.batch_size
+    batch_size = 1000
     database = citations_db
-    # author_operations = Operations().author
     collection = "paper"  # author_operations.collection
     model = fasttext.load_model("models/fasttext_vectorizer.bin")
 
@@ -57,11 +56,17 @@ def set_author_embedding(config_path: str):
         current_time = datetime.now() - init_time
         current_count = batch_cnt * batch_size
         progress = current_count / OBJECTS_COUNT
-        database_changer_logger.info(f'[{current_time}] '
-                                     f'progress: {progress * 100:3.2f}%, '
-                                     f'documents uploaded: {current_count}, '
-                                     f'speed: {current_count / current_time.total_seconds():.2f} obj/sec')
+        logger.debug(f'progress: {progress * 100:3.2f}%, '
+                     f'documents uploaded: {current_count}, '
+                     f'speed: {current_count / current_time.total_seconds():.2f} obj/sec')
 
 
 if __name__ == '__main__':
+    logger.setLevel(level=logging.DEBUG)
+
+    handler = DeltaTimeHandler()
+    formatter = DeltaTimeFormatter("+%(delta)s - %(asctime)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.propagate = False
     set_author_embedding()
